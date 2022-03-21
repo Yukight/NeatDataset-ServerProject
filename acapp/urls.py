fb8d9a46b8b4bbdb1e2cffb0852a7ef4cea32a91
gouqichun <525458513@qq.com>
2022-03-21 23:00:43 +0800

    restframework

--- acapp/urls.py
+++ acapp/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     #path('<str:module>/', views.SysView.as_View()),
     path('page/NeatDataset', views.page_view_ND),
+    path('ckeditor/', include('ckeditor_uploader.urls')),  # 配置富文本编辑器url
+    path('docs/', include_docs_urls(title='DRF文档')),
+    path('Users/', GoodsListView.as_view(), name='goods_list'),
 ]
